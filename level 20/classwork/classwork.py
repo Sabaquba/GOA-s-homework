c43bcf2617d52b072434ae17f07a8c8100308f9c
Saba quba <sabaqubadze@gmail.com>
2024-08-05 04:29:24 -0700

    commit

---
+++ level 20/classwork/classwork.py
@@ -0,0 +1,8 @@
+def greet (saba):
+    print("hello saba")
+    def add (a,+ b):
+    # მაგალითის გამოყენება 
+    num1 = 5
+    num2 = 3
+    result = add(num1, num2,)
+    print("ჯამი არის: [result]")
---
+++ level 23/classwork/classwork.py
@@ -0,0 +1,17 @@
+def list(n,):
+    f_list = []
+    for i in n:
+        if i % 2 != 0:
+            f_list,append()
+            else: 
+                pass
+        return sum (f_list)
+print (list [1,2,3,4,5,6,7])
+def homework(list):
+    integer_nums = []
+    for num in list:
+        if num = int (num):
+            integer_nums.append(num)
+            return integer_nums
+            list[1,2,3,4,5,6,7,8,9,10]
+            print(homework)(list)
